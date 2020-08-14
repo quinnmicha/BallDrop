e6e0acb595f5c115ef6dd47cbc50596a3ebf82f8
quinnmicha <mquinn@email.neit.edu>
2020-08-14 14:03:38 -0400

    Added a spawner using a coroutine

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _ball;
+
+    private float spawnSpeed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(spawnBall(spawnSpeed));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private IEnumerator spawnBall(float spawnSpeed) {
+        while (true) {
+            
+            GameObject newBall = Instantiate(_ball, new Vector2(this.transform.position.x, this.transform.position.y), Quaternion.identity);
+            yield return new WaitForSeconds(spawnSpeed);    
+        }
+    }
+}
